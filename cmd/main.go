c72d8e8aa23818fc29c0d97086bd57477c4a1823
vetrovosk <rpiratskij@mail.ru>
2022-11-19 16:13:01 +0300

    user registration:the beginning

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"log"
-	"os"
-
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"time"
+)
+
+const (
+	shortForm = "2006-Jan-02"
+)
+
+type user struct {
+	birth time.Time
+	sex   string
+	city  string
+}
+
+var gender = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("male"),
+		tgbotapi.NewKeyboardButton("female"),
+	),
 )
 
 func main() {
-	// вот енв лучше вынести отдельно, потому что очевидно, что если у нас нев пусто(==""), то кто-то что-то не заполнил и надо просто заканчивать работу.
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("os.GetENV()"))
+	bot, err := tgbotapi.NewBotAPI("GetENV()")
 	if err != nil {
 		panic(err)
 	}
@@ -17,53 +32,62 @@ func main() {
 	bot.Debug = true
 
 	updateConfig := tgbotapi.NewUpdate(0)
-	// такое выносится в константы или конфиг
 	updateConfig.Timeout = 30
 
+	cahe := make(map[int64]user)
 	updates := bot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
-		// Telegram can send many types of updates depending on what your Bot
-		// is up to. We only want to look at messages for now, so we can
-		// discard any other updates.
-		if update.Message == nil {
+		fmt.Println("map:", cahe)
+
+		_, find := cahe[update.Message.From.ID]
+		if !find {
+			///регистрируйся падла давай
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ты теперь в Матрице")
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+			cahe[update.Message.From.ID] = user{}
+			got := tgbotapi.NewMessage(update.Message.Chat.ID, "Дата рождения?(В форме: 2006-Jan-02)")
+			if _, err := bot.Send(got); err != nil {
+				panic(err)
+			}
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		value := cahe[update.Message.From.ID]
 
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		if _, err := bot.Send(msg); err != nil {
-			// кажется тут нет смысла паниковать, надо залогировать ошибку и пойти на след цикл, ну кому-то не смогли ответить...
-			// очень жаль. по хорошему у нас должен быть механизм ретраев, если не вышло сейчас овтетить. но это все потом
-			panic(err)
+		if time.Time.IsZero(value.birth) == true {
+			value.birth, _ = time.Parse(shortForm, update.Message.Text)
+			cahe[update.Message.From.ID] = value
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Твой пол?")
+			msg.ReplyMarkup = gender
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+			continue
 		}
+
+		if value.sex == "" {
+			value.sex = update.Message.Text
+			cahe[update.Message.From.ID] = value
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Твой город?")
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+			continue
+		}
+
+		if value.city == "" {
+			value.city = update.Message.Text
+			cahe[update.Message.From.ID] = value
+		}
+		break
 	}
 }
 
-
-//
-func main2() {
-	// подгатавливаем данные
-	// считываем конфиги
-	// читаем енв
-	// настраиваем уровень логирование
-	// и потом начинаем наше приложение что-то типа
-	data = {}
-
-	// потом инициализируем ботаcon
-	bot, err  := NewBot(data)
-	if err != nil {
-		log.Panicf("can't init bot, error: %s", err)
-	}
-	bot.Run()
-
-	// то есть мейн такой голенький
-	// It's common to have a small main function that imports and invokes the code from the /internal and /pkg directories and nothing else.
-
-
-	// сам бот будет жить где-то в папке internal
-	// https://github.com/golang-standards/project-layout
-
-}
+//потом сделать
+// кажется тут нет смысла паниковать, надо залогировать ошибку и пойти на след цикл, ну кому-то не смогли ответить...
+// очень жаль. по хорошему у нас должен быть механизм ретраев, если не вышло сейчас овтетить. но это все потом
